91b5d7fcd0ebdc7787584c00a1a469c101394c35
Irmischer <h.irmischer@francotyp.com>
2016-08-22 11:15:40 +0200

    1. Adjusted gulp tasks 2. Supressing transpilation by VS

--- Ng2NodeTemplate/gulpfile.js
+++ Ng2NodeTemplate/gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require("gulp");
 var del = require("del");
-const tslint = require('gulp-tslint');
+var ts = require('gulp-typescript');
+var merge = require('merge2');  // Require separate installation
+const tslint = require("gulp-tslint"); 
 
-gulp.task('tslint', function () {
-    return gulp.src('src/**/*.ts')
+// runs ts lint with settings defined tslint.json
+gulp.task("ts:lint", function () {
+    return gulp.src("src/**/*.ts")
         .pipe(tslint({
             formatter: "verbose"
         }))
@@ -11,27 +14,46 @@ gulp.task('tslint', function () {
 });
 
 // clean the contents of the distribution directory
-gulp.task('clean', function () {
-    return del(['public/app/**/*', 'public/lib/**/*' ]);
+gulp.task("ts:clean:libs", function () {
+    return del("public/lib/**/*");
 });
 
-// copy dependencies
-gulp.task('copy:libs', ['clean'], function () {
+gulp.task("ts:clean:src", function () {
+    return del("public/app/**/*");
+});
+
+// copy polyfills
+gulp.task("ng2:polyfills", function () {
     return gulp.src([
-        'node_modules/core-js/client/shim.min.js',
-        'node_modules/zone.js/dist/zone.js',
-        'node_modules/reflect-metadata/Reflect.js',
-        'node_modules/systemjs/dist/system.src.js'
+        "node_modules/core-js/client/shim.min.js",
+        "node_modules/zone.js/dist/zone.js",
+        "node_modules/reflect-metadata/Reflect.js",
+        "node_modules/systemjs/dist/system.src.js"
     ])
-        .pipe(gulp.dest('public/lib'));
+        .pipe(gulp.dest("public/lib"));
 });
 
-// TypeScript compile
-//gulp.task('compile', ['clean'], function () {
-//    return gulp
-//        .src('src/**/*.ts')
-//        .pipe(typescript(tscConfig.compilerOptions))
-//        .pipe(gulp.dest('public/app'));
-//});
+// copy ng2 and dependencies
+gulp.task("ng2:libs", ["ng2:polyfills"], function () {
+    return gulp.src([
+        "node_modules/@angular/**/*.js",
+        "node_modules/@angular/**/*.map",
+        "node_modules/rxjs/**/*.js",
+        "node_modules/rxjs/**/*.map"
+    ], { base: './node_modules/' })
+        .pipe(gulp.dest("public/lib"));
+});
 
-gulp.task('build', ['clean', 'tslint', 'copy:libs']);
+// transpile typescript files into output folder
+var tsProject = ts.createProject('tsconfig.json');
+gulp.task('ts:build', ["ts:lint", "ts:clean:src"], function () {
+    var tsResult = gulp.src('src/**/*.ts')
+        .pipe(ts(tsProject));
+
+    return merge([
+        tsResult.dts.pipe(gulp.dest('public/definitions')),
+        tsResult.js.pipe(gulp.dest('public/app'))
+    ]);
+});
+
+gulp.task("ts:rebuild", ["ts:lint", "ts:clean:libs", "ts:clean:src", "ng2:libs", "ts:build"]);
--- Ng2NodeTemplate/src/app.module.ts
+++ Ng2NodeTemplate/src/app.module.ts
@@ -1,10 +1,11 @@
-﻿import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }  from './app.component';
+﻿import { NgModule }      from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppComponent }  from "./app.component";
 
 @NgModule({
-    imports: [BrowserModule],
+    bootstrap: [AppComponent],
     declarations: [AppComponent],
-    bootstrap: [AppComponent]
+    imports: [BrowserModule],
 })
 export class AppModule { }
